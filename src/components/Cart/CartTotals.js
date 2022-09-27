f42c348de39bb9e0704105f995e56677ba9d695d
rocky deng <xiwe001@163.com>
2022-09-27 23:00:04 +0800

    fix warnings

--- src/components/Cart/CartTotals.js
+++ src/components/Cart/CartTotals.js
@@ -1,5 +1,4 @@
 import React, { Component } from "react";
-import PayPalButton from "./PayPalButton";
 import { Link } from "react-router-dom";
 export default class CartTotals extends Component {
   render() {
@@ -10,7 +9,7 @@ export default class CartTotals extends Component {
       cart,
       clearCart
     } = this.props.value;
-    const { history } = this.props;
+    
     const emptyCart = cart.length === 0 ? true : false;
     return (
       <React.Fragment>
